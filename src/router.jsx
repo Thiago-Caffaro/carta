7c69bb131b15c5749640545f789f1273e626a645
Cleber Lima <thiagocaffaro223@gmail.com>
2023-12-30 12:51:03 -0300

    router alterado

--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Carta from './pages/carta.jsx';
 import Cards from './pages/cards.jsx';
 import Canvas from './pages/canvas.jsx';
 
 const AppRouter = () => {
   return (
-    <Router basename="/carta">
+    <Router>
       <Routes>
-        <Route path="/" element={<Carta />} />
+        <Route path="/cartinha" element={<Carta />} />
         <Route path="/card" element={<Cards />} />
         <Route path="/canvas" element={<Canvas />} />
       </Routes>
